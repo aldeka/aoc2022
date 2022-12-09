0283b097d4eb0c61ddf57980904bb7a007aefa18
Karen Rustad Tölva <karen.rustad@gmail.com>
2022-12-08 22:34:34 -0800

    Started day 9

--- src/main.rs
+++ src/main.rs
@@ -1,123 +1,89 @@
-use grid::*;
-use std::fs;
+use std::{collections::HashSet, fs};
 
-#[derive(Debug)]
-struct Tree {
-    height: u32,
-    is_seen: bool,
+type Position = (i32, i32);
+
+fn next_knot_position(head: Position, tail: Position) -> Position {
+    let mut next_position = (tail.0, tail.1);
+    let mut horiz_move = false;
+    if (head.0 - tail.0).abs() > 1 {
+        horiz_move = true;
+        next_position.0 += (head.0 - tail.0).signum();
+    }
+    let mut vert_move = false;
+    if (head.1 - tail.1).abs() > 1 {
+        vert_move = true;
+        next_position.1 += (head.1 - tail.1).signum();
+    }
+
+    if horiz_move && next_position.1 != head.1 {
+        // shift vert too, because diagonal move
+        next_position.1 += (head.1 - tail.1).signum();
+    } else if vert_move && next_position.0 != head.0 {
+        // shift horiz too, because diagonal move
+        next_position.0 += (head.0 - tail.0).signum();
+    }
+
+    next_position
 }
 
-fn make_tree_grid(contents: &str) -> Grid<Tree> {
-    let column_count = contents.split("\n").next().unwrap().len();
+fn simulate_rope(contents: &str, knot_count: u32) -> usize {
+    let mut visited_positions = HashSet::<Position>::new();
+    let mut knots = Vec::<Position>::new();
 
-    let grid_contents = contents
-        .chars()
-        .filter(|char| char.to_string() != "\n".to_string())
-        .map(|char| Tree {
-            height: char.to_digit(10).unwrap(),
-            is_seen: false,
-        })
-        .collect();
+    let mut i = 0;
+    // initialize knot list
+    while i < knot_count {
+        knots.push((0, 0));
+        i += 1;
+    }
 
-    let grid: Grid<Tree> = Grid::from_vec(grid_contents, column_count);
-    // println!("Grid {:#?}", grid.size());
-    grid
-}
+    visited_positions.insert(knots[knots.len() - 1].clone()); // because we start there
 
-fn get_row_visible_tree_count<'a>(row: impl Iterator<Item = &'a mut Tree>) -> u32 {
-    let mut count = 0;
-    let mut tallest_tree: Option<u32> = None;
-    for tree in row {
-        if tallest_tree == None || tree.height > tallest_tree.unwrap() {
-            tallest_tree = Some(tree.height);
-            if !tree.is_seen {
-                count += 1;
-                tree.is_seen = true;
+    for line in contents.split("\n") {
+        let parts: Vec<&str> = line.split(" ").collect();
+        if parts.len() == 2 {
+            let direction = parts[0];
+            let mut count = parts[1].parse::<i32>().unwrap();
+            // println!("Move: {} {}", direction, count);
+
+            while count > 0 {
+                // move the head of the rope
+                match direction {
+                    "L" => knots[0].0 -= 1,
+                    "R" => knots[0].0 += 1,
+                    "D" => knots[0].1 -= 1,
+                    "U" => knots[0].1 += 1,
+                    _ => panic!("Unrecognized direction {direction}"),
+                }
+                // then move the subsequent knots one at a time
+                let mut i = 1;
+                while i < knots.len() {
+                    knots[i] = next_knot_position(knots[i - 1], knots[i]);
+                    i += 1;
+                }
+                // and record the tail position
+                visited_positions.insert(knots[knots.len() - 1].clone());
+                count -= 1;
             }
         }
     }
-    count
-}
-
-fn get_total_visible_tree_count(g: &mut Grid<Tree>) -> u32 {
-    let mut visible_count = 0;
-    // top and bottom
-    let mut i = 0;
-    while i < g.rows() {
-        let row = g.iter_row_mut(i);
-        visible_count += get_row_visible_tree_count(row);
-        let rev_row = g.iter_row_mut(i).rev();
-        visible_count += get_row_visible_tree_count(rev_row);
-        i += 1;
-    }
-    // left and right;
-    i = 0;
-    while i < g.cols() {
-        let col = g.iter_col_mut(i);
-        visible_count += get_row_visible_tree_count(col);
-        let rev_col = g.iter_col_mut(i).rev();
-        visible_count += get_row_visible_tree_count(rev_col);
-        i += 1;
-    }
-    visible_count
-}
-
-fn trees_from_point<'a>(row: Vec<&Tree>, start: usize) -> u32 {
-    let mut count = 0;
-    let start_height = row[start].height;
-    let mut x = start + 1;
-
-    while x < row.len() {
-        count += 1;
-        if row[x].height >= start_height {
-            break;
-        }
-        x += 1;
-    }
-
-    count
+    // println!("{:#?}", visited_positions);
+    visited_positions.len()
 }
 
 fn part1(contents: &str) {
     println!("Part 1");
-    let mut grid = make_tree_grid(contents);
-    let visible_count = get_total_visible_tree_count(&mut grid);
-
-    println!("Visible tree count:\n{}", visible_count);
+    println!("Tail visitation count: {}", simulate_rope(contents, 2));
 }
 
 fn part2(contents: &str) {
     println!("Part 2");
-    let grid = make_tree_grid(contents);
-    let mut max_scenic_score = 0;
-
-    let mut i = 0;
-
-    while i < grid.rows() {
-        let mut j = 0;
-        while j < grid.cols() {
-            // let tree = grid.get(i, j).unwrap();
-            // println!("({},{}): {}", i, j, tree.height);
-            let north_count =
-                trees_from_point(grid.iter_row(j).rev().collect(), grid.cols() - 1 - i);
-            let south_count = trees_from_point(grid.iter_row(j).collect(), i);
-            let west_count =
-                trees_from_point(grid.iter_col(i).rev().collect(), grid.cols() - 1 - j);
-            let east_count = trees_from_point(grid.iter_col(i).collect(), j);
-            let score = north_count * south_count * west_count * east_count;
-            if score > max_scenic_score {
-                max_scenic_score = score;
-            }
-            j += 1;
-        }
-        i += 1;
-    }
-    println!("Max scenery:\n{}", max_scenic_score);
+    println!("Tail visitation count: {}", simulate_rope(contents, 10));
 }
 
 fn main() {
     let contents =
-        fs::read_to_string("src/08/input.txt").expect("Should have been able to read the file");
+        fs::read_to_string("src/09/input.txt").expect("Should have been able to read the file");
     part1(&contents);
     println!("");
     part2(&contents);
@@ -128,53 +94,106 @@ mod tests {
     use crate::*;
 
     #[test]
-    fn test_get_row_visible_tree_count() {
-        let sample_input = "30373
-25512
-65332
-33549
-35390";
-        let mut g = make_tree_grid(sample_input);
-        let visible_tree_count = get_row_visible_tree_count(g.iter_row_mut(0));
-        assert_eq!(visible_tree_count, 2);
-        // only find newly-visible trees when we run it again
-        let opposite_visible_tree_count = get_row_visible_tree_count(g.iter_row_mut(0).rev());
-        assert_eq!(opposite_visible_tree_count, 1);
+    fn test_next_knot_position() {
+        // up
+        let head: Position = (0, 2);
+        let tail: Position = (0, 0);
+        let mut next: Position = next_knot_position(head, tail);
+        assert_eq!(next, (0, 1));
+
+        // down
+        let head: Position = (0, -3);
+        let tail: Position = (0, -1);
+        next = next_knot_position(head, tail);
+        assert_eq!(next, (0, -2));
+
+        // right
+        let head: Position = (0, 0);
+        let tail: Position = (-3, 0);
+        next = next_knot_position(head, tail);
+        assert_eq!(next, (-2, 0));
+
+        // left
+        let head: Position = (3, 0);
+        let tail: Position = (5, 0);
+        next = next_knot_position(head, tail);
+        assert_eq!(next, (4, 0));
+
+        // diagonal
+        let head: Position = (2, -1);
+        let tail: Position = (0, 0);
+        next = next_knot_position(head, tail);
+        assert_eq!(next, (1, -1));
+
+        // another diagonal
+        let head: Position = (2, 3);
+        let tail: Position = (1, 1);
+        next = next_knot_position(head, tail);
+        assert_eq!(next, (2, 2));
+
+        // another diagonal
+        let head: Position = (4, 1);
+        let tail: Position = (3, 0);
+        next = next_knot_position(head, tail);
+        assert_eq!(next, (3, 0));
+        // another diagonal
+        let head: Position = (4, 2);
+        let tail: Position = (3, 0);
+        next = next_knot_position(head, tail);
+        assert_eq!(next, (4, 1));
     }
 
     #[test]
-    fn test_get_col_visible_tree_count() {
-        let sample_input = "30373
-25512
-65332
-33549
-35390";
-        let mut g = make_tree_grid(sample_input);
-        println!("{:#?}", g);
-        let mut col = g.iter_col_mut(1);
-        let visible_tree_count = get_row_visible_tree_count(&mut col);
-        println!("{:#?}", col);
-        assert_eq!(visible_tree_count, 2);
-        // only find newly-visible trees when we run it again
-        let mut col_rev = g.iter_col_mut(1).rev();
-        let opposite_visible_tree_count = get_row_visible_tree_count(&mut col_rev);
-        println!("{:#?}", col_rev);
-        assert_eq!(opposite_visible_tree_count, 1);
+    fn test_simulate_rope() {
+        let input = "R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2";
+        let visited_positions = simulate_rope(input, 2);
+        assert_eq!(visited_positions, 13);
     }
 
     #[test]
-    fn test_get_visible_tree_count() {
-        //         let sample_input = "30373
-        // 25512
-        // 65332
-        // 33549
-        // 35390";
-        let sample_input = "12345
-12145
-12345";
-        let mut g = make_tree_grid(sample_input);
-        let visible_tree_count = get_total_visible_tree_count(&mut g);
-        println!("{:#?}", g);
-        assert_eq!(visible_tree_count, 14);
+    fn test_basic_long_rope() {
+        let input = "R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2";
+        let visited_positions = simulate_rope(input, 10);
+        assert_eq!(visited_positions, 1);
+    }
+
+    #[test]
+    fn test_advanced_long_rope() {
+        let input = "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20";
+        let visited_positions = simulate_rope(input, 10);
+        assert_eq!(visited_positions, 36);
+    }
+
+    #[test]
+    fn test_hashset() {
+        let mut hs = HashSet::<Position>::new();
+        let blah: Position = (0, 1);
+        hs.insert(blah.clone());
+        hs.insert(blah.clone());
+        hs.insert((1, 3));
+        hs.insert((0, 1));
+        hs.insert((3, 1));
+        assert_eq!(hs.len(), 3);
     }
 }
