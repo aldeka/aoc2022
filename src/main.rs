a281c4b2eba38ca489d8fc715a09d83b904d930f
Karen Rustad Tölva <karen.rustad@gmail.com>
2022-12-11 20:44:01 -0800

    Code style bits

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, collections::VecDeque, num::ParseIntError};
+use std::{collections::VecDeque, fs};
 
 struct Monkey {
     name: usize,
@@ -12,15 +12,15 @@ struct Monkey {
 }
 
 impl Monkey {
-    fn new(name: usize,
+    fn new(
+        name: usize,
         items: VecDeque<usize>,
         on_inspect: Box<dyn Fn(usize) -> usize>,
         test_divisor: usize,
         throw_to_true: usize,
         throw_to_false: usize,
-        lcm: Option<usize>
-    ) -> Monkey
-    {
+        lcm: Option<usize>,
+    ) -> Monkey {
         Monkey {
             name,
             items,
@@ -29,7 +29,7 @@ impl Monkey {
             throw_to_true,
             throw_to_false,
             inspect_count: 0,
-            lcm
+            lcm,
         }
     }
 
@@ -40,7 +40,11 @@ impl Monkey {
     fn inspect(&mut self) {
         self.inspect_count += self.items.len();
         Some(self.lcm).expect("LCM not found!");
-        self.items = self.items.iter().map(|item| (self.on_inspect)(*item) % self.lcm.unwrap()).collect();
+        self.items = self
+            .items
+            .iter()
+            .map(|item| (self.on_inspect)(*item) % self.lcm.unwrap())
+            .collect();
     }
 
     fn throw(&mut self) -> usize {
@@ -66,39 +70,33 @@ impl Monkey {
 //     If true: throw to monkey 4
 //     If false: throw to monkey 7
 
-
 fn make_operation(operation_line: &str) -> Box<dyn Fn(usize) -> usize> {
     if operation_line.find("+") != None {
         // addition, which always has a static side to it
-        let static_val = operation_line.split(" ")
-            .find(|operand| {
-                match operand.parse::<usize>() {
-                    Ok(x) => return true,
-                    Err(_) => return false,
-                }
+        let static_val = operation_line
+            .split(" ")
+            .find(|operand| match operand.parse::<usize>() {
+                Ok(_) => return true,
+                Err(_) => return false,
             })
-            .unwrap().parse::<usize>().unwrap();
-        return Box::new(move |b: usize| -> usize {
-            static_val + b
-        });
+            .unwrap()
+            .parse::<usize>()
+            .unwrap();
+        return Box::new(move |b: usize| -> usize { static_val + b });
     } else {
         // it's a multiplication one
-        let static_val_str = operation_line.split(" ")
-        .find(|operand| {
-            match operand.parse::<usize>() {
-                Ok(x) => return true,
-                Err(_) => return false,
-            }
-        });
+        let static_val_str =
+            operation_line
+                .split(" ")
+                .find(|operand| match operand.parse::<usize>() {
+                    Ok(_) => return true,
+                    Err(_) => return false,
+                });
         if static_val_str != None {
             let static_val = static_val_str.unwrap().parse::<usize>().unwrap();
-            return Box::new(move |b: usize| -> usize {
-                b * static_val
-            });
+            return Box::new(move |b: usize| -> usize { b * static_val });
         } else {
-            return Box::new(move |b: usize| -> usize {
-                b * b
-            });
+            return Box::new(move |b: usize| -> usize { b * b });
         }
     }
 }
@@ -107,34 +105,52 @@ fn parse_monkeys(contents: &str) -> Vec<Monkey> {
     let mut monkeys: Vec<Monkey> = Vec::new();
     let mut primes: Vec<usize> = Vec::new();
     for monkey_spec in contents.split("\n\n") {
-        let lines: Vec<&str> = monkey_spec.split("\n")
+        let lines: Vec<&str> = monkey_spec
+            .split("\n")
             .filter(|line| line != &"")
             .collect::<Vec<&str>>();
-        let name: usize = lines[0].trim().split(" ")
+        let name: usize = lines[0]
+            .trim()
+            .split(" ")
             .filter(|line| {
                 return line != &"" && line != &"Monkey";
             })
             .map(|line| line.replace(":", ""))
             .collect::<Vec<String>>()[0]
-            .parse::<usize>().unwrap();
-        let starting_items: VecDeque<usize> = lines[1].trim().split("Starting items: ")
+            .parse::<usize>()
+            .unwrap();
+        let starting_items: VecDeque<usize> = lines[1]
+            .trim()
+            .split("Starting items: ")
             .collect::<VecDeque<&str>>()[1]
             .split(", ")
-            .map(|item| item.parse::<usize>().unwrap()).collect();
+            .map(|item| item.parse::<usize>().unwrap())
+            .collect();
         println!("Monkey {name}: {starting_items:#?}");
         println!("***");
 
-        let operation = make_operation(
-            lines[2].replace("Operation: ", "")
-            .as_str()
-        );
-        let test_divisor: usize = lines[3]["  Test: divisible by ".len()..].parse::<usize>().unwrap();
+        let operation = make_operation(lines[2].replace("Operation: ", "").as_str());
+        let test_divisor: usize = lines[3]["  Test: divisible by ".len()..]
+            .parse::<usize>()
+            .unwrap();
         primes.push(test_divisor);
-        let throw_to_true: usize = lines[4]["    If true: throw to monkey ".len()..].parse::<usize>().unwrap();
-        let throw_to_false: usize = lines[5]["    If false: throw to monkey ".len()..].parse::<usize>().unwrap();
+        let throw_to_true: usize = lines[4]["    If true: throw to monkey ".len()..]
+            .parse::<usize>()
+            .unwrap();
+        let throw_to_false: usize = lines[5]["    If false: throw to monkey ".len()..]
+            .parse::<usize>()
+            .unwrap();
 
         let lcm = None::<usize>;
-        let monkey = Monkey::new(name, starting_items, operation, test_divisor, throw_to_true, throw_to_false, lcm);
+        let monkey = Monkey::new(
+            name,
+            starting_items,
+            operation,
+            test_divisor,
+            throw_to_true,
+            throw_to_false,
+            lcm,
+        );
         monkeys.push(monkey);
     }
     let lcm = primes.iter().product();
@@ -161,9 +177,15 @@ fn part2(contents: &str) {
             println!("Monkey {}: {:#?}", monkeys[i].name, monkeys[i].items);
         }
     }
-    let mut inspect_counts = monkeys.iter().map(|monk| monk.inspect_count).collect::<Vec<usize>>();
+    let mut inspect_counts = monkeys
+        .iter()
+        .map(|monk| monk.inspect_count)
+        .collect::<Vec<usize>>();
     inspect_counts.sort();
-    println!("Answer: {}", inspect_counts.pop().unwrap() * inspect_counts.pop().unwrap());
+    println!(
+        "Answer: {}",
+        inspect_counts.pop().unwrap() * inspect_counts.pop().unwrap()
+    );
 }
 
 fn main() {
